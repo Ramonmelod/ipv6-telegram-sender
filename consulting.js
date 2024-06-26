c2edc5e496b98e5fec2b9a34227262b1e8b00938
ramonmelod <ramonmelo.com@gmail.com>
2024-06-26 00:17:01 -0300

    implementation of the send of the ipv6 to telegram

--- consulting.js
+++ consulting.js
@@ -1,13 +1,15 @@
-const loop = setInterval(async () => {
+const loop = async () => {
   try {
     const response = await fetch("https://api64.ipify.org?format=json");
     if (!response.ok) {
       throw new Error("internet response is not ok");
     }
     const data = await response.json();
-    console.log(data.ip);
+    const ip = data.ip;
+    console.log(ip);
+    return { ip };
   } catch (error) {
     console.error("there was a problem in the fetch operation", error);
   }
-}, 3000);
+};
 module.exports = { loop };
--- telegram.js
+++ telegram.js
@@ -1,34 +1,51 @@
 const consulting = require("./consulting");
 const dotenv = require("dotenv").config();
+
 const botToken = process.env.BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
 
-async () => {
-  const query = await consulting.loop(); // Call the loop function to get data
-  console.log(query.ip); // Log the IP address fetched
-};
-const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-
-const text = "Esta é uma mensagem de teste enviada usando fetch.";
-
-const data = {
-  chat_id: chatId,
-  text,
-};
-
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
+const search = async () => {
+  try {
+    const query = await consulting.loop(); // Call the loop function to get data
+    const ip = query.ip;
+    console.log("IP:", ip); // Log the IP address fetched
+    return ip; // Return the IP address from the search function
+  } catch (error) {
+    console.error("Error fetching IP:", error);
+    throw error; // Throw any errors encountered during the search
+  }
 };
 
-fetch(url, options)
-  .then((response) => response.json())
-  .then((responseData) => {
-    console.log("Success:", responseData);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+const telegramSender = async () => {
+  try {
+    // Call search function to get the IP
+    const ip = await search();
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const text = `IPV6: ${ip}`; // Include the IP in the message text
+    const data = {
+      chat_id: chatId,
+      text,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error("Failed to send message to Telegram");
+    }
+
+    const responseData = await response.json();
+    console.log("Message sent successfully:", responseData);
+  } catch (error) {
+    console.error("Error sending message to Telegram:", error);
+  }
+};
+
+// Call telegramSender function to send the message
+telegramSender();
